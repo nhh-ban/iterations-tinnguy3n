86dfb298b2fa973e13d1ff1a2aa3a740eb7b137a
Tin Nguyen <tin.nguyen123@hotmail.com>
2023-10-11 16:26:51 +0200

    Problem 2 and 4 done

--- functions/data_transformations.r
+++ functions/data_transformations.r
@@ -1,4 +1,6 @@
-# Data transformations file - Problem 2
+# Data transformations file
+
+# Problem 2
 
 # Unlist function
 unlist_safe <-
@@ -12,16 +14,45 @@ unlist_safe <-
 
 # Function transforming metadata to dataframe
 transform_metadata_to_df <- 
-  function(y) {
-    {y <- stations_metadata[[1]]}
-    {stations_metadata[[1]] %>%
+  function(df) {
+    df <-
+      stations_metadata[[1]] %>%
       map(as_tibble) %>% 
       list_rbind() %>% 
       mutate(latestData = map_chr(latestData, unlist_safe)) %>% 
       mutate(latestData = as_datetime(latestData, tz = "UTC")) %>% 
       unnest_wider(location) %>% 
-      unnest_wider(latLon)}
-  }
-  
-transform_metadata_to_df(.)
+      unnest_wider(latLon)
+    
+    return(df) # Returns the data frame in the function
+    }
 
+
+# Problem 4
+
+# a - time function
+to_iso8601 <- function(datetime, offset_days) {
+  
+  # Converts string to datetime object
+  datetime_var <- anytime(datetime)
+  
+  # Add offset to original time
+  adjusted_datetime <- datetime_var + days(offset_days)
+  
+  # UTC and add iso8601 formatting with Z
+  iso8601_z <- format(adjusted_datetime, 
+                      format = "%Y-%m-%dT%H:%M:%SZ",
+                      tz = "UTC")
+  
+  return(iso8601_z)
+}
+
+# Test the time function
+to_iso8601(as_datetime("2016-09-01 10:11:12"), 0)   # Works
+to_iso8601(as_datetime("2016-09-01 10:11:12"), -4)  # Works
+
+
+
+
+    
+  
